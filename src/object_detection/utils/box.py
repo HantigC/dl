a32a8f1c205ce57004e90fb21d4a56ecf69a7dc9
HantigC <cristianhantig96@gmail.com>
2023-09-20 18:07:23 +0300

    add bounding box visualization

--- src/object_detection/utils/box.py
+++ src/object_detection/utils/box.py
@@ -1,11 +1,12 @@
 import torch
+import numpy as np
 
 
 def make_grid(grid_size, height=1, width=1):
     h, w = grid_size
     xs = torch.linspace(0, width, w + 1)
     ys = torch.linspace(0, height, h + 1)
-    ws = torch.stack(torch.meshgrid(ys, xs), dim=2)
+    ws = torch.stack(torch.meshgrid(ys, xs, indexing="ij"), dim=2)
     grid = torch.cat([ws[:-1, :-1], ws[1:, 1:]], dim=-1).view(-1, 4)
     return grid
 
@@ -25,3 +26,17 @@ def compute_iou_tl_br(st_tlbr, nd_tlbr):
 
 def compute_area(yxyx):
     return (yxyx[:, 2:] - yxyx[:, :2]).prod(1)
+
+
+def yxyx_to_xywh(yxyx):
+    yxyx = np.array(yxyx)
+    hw = yxyx[:, 2:] - yxyx[:, :2]
+    xywh = np.stack([yxyx[:, 1], yxyx[:, 0], hw[:, 1], hw[:, 0]], axis=-1)
+    return xywh
+
+
+def xywh_to_yxyx(xywh):
+    xywh = np.array(xywh)
+    wh = xywh[:, 2:] + xywh[:, :2]
+    yxyx = np.concatenate([xywh[:, 1], xywh[:, 0], wh[:, 1], wh[:, 0]], axis=-1)
+    return yxyx
---
+++ src/object_detection/utils/visualization.py
@@ -0,0 +1,108 @@
+from typing import Sequence, Tuple, Union, Optional
+import cv2
+import numpy as np
+from .box import make_grid, xywh_to_yxyx
+import itertools as it
+
+
+def draw_grid_(
+    img: np.ndarray,
+    grid: Sequence[Sequence[int]] = None,
+    grid_size: Tuple[int, int] = None,
+) -> None:
+    if grid is None:
+        if grid_size is None:
+            raise ValueError(
+                "At least one of `grid_size` and `grid` should be provided"
+            )
+
+        grid = make_grid(grid_size)
+
+    h, w, _ = img.shape
+    for tl_y, tl_x, br_y, br_x in grid:
+        cv2.rectangle(
+            img,
+            (int(w * tl_x), int(h * tl_y)),
+            (int(w * br_x), int(h * br_y)),
+            (255, 0, 0),
+        )
+
+
+def draw_rect_xywh_(
+    img: np.ndarray,
+    tl_x: Union[int, float],
+    tl_y: Union[int, float],
+    w: Union[int, float],
+    h: Union[int, float],
+    color: Tuple[int, int, int],
+    **kwargs,
+) -> None:
+    tl_x = int(tl_x)
+    tl_y = int(tl_y)
+    w = int(w)
+    h = int(h)
+    cv2.rectangle(img, (tl_x, tl_y), (tl_x + w, tl_y + h), color=color, **kwargs)
+
+
+def draw_bbox_xyhw(
+    img: np.ndarray,
+    category: str,
+    tl_x: Union[int, float],
+    tl_y: Union[int, float],
+    w: Union[int, float],
+    h: Union[int, float],
+    box_color: Tuple[int, int, int],
+    text_color: Tuple[int, int, int],
+    **kwargs,
+) -> None:
+    draw_rect_xywh_(img, tl_x, tl_y, w, h, box_color, **kwargs)
+    cv2.putText(img, category, (int(tl_x), int(tl_y)), 1, 1, text_color)
+
+
+def draw_boxes_tlbr(
+    img,
+    boxes: Sequence[Sequence[Union[int, float]]],
+    colors: Optional[Union[Sequence[int], Sequence[Sequence[int]]]] = None,
+    scale: bool = True,
+    **kwargs,
+) -> None:
+    if colors is None:
+        colors = np.random.randint(255, size=(len(boxes), 3)).tolist()
+    elif isinstance(colors, list):
+        assert len(boxes) == len(
+            colors
+        ), "`boxes` and `colors` should hange the same length"
+        if not isinstance(colors[0], (list, tuple)):
+            colors = it.cycle([colors])
+    else:
+        raise ValueError(
+            f"`colors` should be a list of colors or a single color, not {colors}"
+        )
+
+    h, w, _ = img.shape
+    for c, (tl_y, tl_x, br_y, br_x) in zip(colors, boxes):
+        if scale:
+            cv2.rectangle(
+                img,
+                (int(w * tl_x), int(h * tl_y)),
+                (int(w * br_x), int(h * br_y)),
+                color=c,
+            )
+        else:
+            cv2.rectangle(
+                img,
+                (int(tl_x), int(tl_y)),
+                (int(br_x), int(br_y)),
+                color=c,
+            )
+
+
+def draw_boxes_xywh(
+    img,
+    boxes: Sequence[Sequence[Union[int, float]]],
+    colors: Optional[Union[Sequence[int], Sequence[Sequence[int]]]] = None,
+    scale: bool = True,
+    **kwargs,
+) -> None:
+    xywh = xywh_to_yxyx(boxes)
+    draw_boxes_tlbr(img, xywh, colors=colors, scale=scale, **kwargs)
